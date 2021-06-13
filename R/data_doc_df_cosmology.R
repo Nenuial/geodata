5e6775bd4716a7cfe29e3a3a4c36ad5a86485990
Pascal Burkhard <pascal.burkhard@gmail.com>
2021-06-13 23:23:50 +0200

    Add Solar System Objects data

---
+++ R/data_doc_df_cosmology.R
@@ -0,0 +1,18 @@
+#' DF Cosmology: Solar System Objects
+#'
+#' A table with solar system objects:
+#' the Sun and the Planets.
+#'
+#' @format A tibble with 8 columns and 10 rows
+#' \describe{
+#'   \item{english}{The english name of the object}
+#'   \item{french}{The french name of the object}
+#'   \item{distance}{The distance to the Sun in AU}
+#'   \item{mass}{The masse in kilogram}
+#'   \item{radius}{The radius in meter}
+#'   \item{density}{The density in kilgram per cubic meters}
+#'   \item{rotation}{The rotation period in hours or days}
+#'   \item{revolution}{The revolution period compared to Earth}
+#' }
+#' @source
+"df_cosmology_solar_system_objects"
---
+++ data-raw/df_cosmology.R
@@ -0,0 +1,9 @@
+# Solar system objects ----------------------------------------------------
+
+df_cosmology_solar_system_objects <- readr::read_csv(here::here("inst/extdata/df_cosmology/solar_system_objects.csv"))
+
+usethis::use_data(df_cosmology_solar_system_objects, overwrite = TRUE)
+
+# Open documentation file -------------------------------------------------
+
+usethis::edit_file(here::here("R/data_doc_df_cosmology.R"))
