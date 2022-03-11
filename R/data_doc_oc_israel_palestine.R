c0a98b33633cf8968c2ac936a7a74de467afde4d
Pascal Burkhard <pascal.burkhard@gmail.com>
2022-03-11 15:08:26 +0100

    Add Israël foreign born population dataset

--- R/data_doc_oc_israel_palestine.R
+++ R/data_doc_oc_israel_palestine.R
@@ -10,3 +10,17 @@
 #' }
 #' @source <https://www.cbs.gov.il/he/publications/doclib/2018/4.%20shnatonimmigration/st04_02.pdf>
 "oc_israel_palestine_immigration_israel"
+
+#' OC Israël-Palestine: Population née à l'étranger
+#'
+#' Un tableau avec l'origine pour la population
+#' née à l'étranger en Israël en 2008.
+#'
+#' @format Un tibble avec 2 colonnes et 5 lignes
+#' \describe{
+#'   \item{country}{Le pays de naissance}
+#'   \item{continent}{Le continent de naissance}
+#'   \item{population}{Nombre absolu de personnes}
+#' }
+#' @source <https://www.cbs.gov.il/en/Statistics/Pages/Generators/Generator-of-the-2008-Census.aspx>
+"oc_israel_palestine_2008_population_nae"
--- data-raw/oc_israel_palestine.R
+++ data-raw/oc_israel_palestine.R
@@ -6,6 +6,38 @@ oc_israel_palestine_immigration_israel <- readr::read_csv(here::here("inst/extda
 
 usethis::use_data(oc_israel_palestine_immigration_israel, overwrite = T)
 
+# Foreign population by origin --------------------------------------------
+
+xls <- here::here("inst/extdata/cbs_israel/2008_population_born_abroad.xlsx")
+fbp <- tidyxl::xlsx_cells(xls)
+fbp_format <- tidyxl::xlsx_formats(xls)
+bold <- fbp_format$local$font$bold
+
+fbp |>
+  dplyr::filter(row >= 10) |>
+  dplyr::filter(col %in% c(2, 24)) |>
+  unpivotr::behead_if(
+    bold[local_format_id],
+    direction = "right-up",
+    name = "continent"
+  ) |>
+  unpivotr::behead("right", name = "country") |>
+  dplyr::select(country, continent, population = numeric) |>
+  tidyr::drop_na() |>
+  dplyr::mutate(population = population * 100) |>
+  dplyr::mutate(
+    country = dplyr::case_when(
+      country == "United States of America" ~ "United States of America and Puerto Rico",
+      country == "India, Pakistan " ~ "India, Pakistan and Sri Lanka",
+      TRUE ~ country
+    )
+  ) |>
+  dplyr::mutate(
+    continent = stringr::str_replace(continent, " - total", "")
+  ) -> oc_israel_palestine_2008_population_nae
+
+usethis::use_data(oc_israel_palestine_2008_population_nae, overwrite = T)
+
 # Open documentation file -------------------------------------------------
 
 usethis::edit_file(here::here("R/data_doc_oc_israel_palestine.R"))
