e8a613b1ba9abdbc356301fc91c70572ba93228a
Pascal Burkhard <pascal.burkhard@gmail.com>
2023-08-16 00:35:16 +0200

    Revert hmd lex function

--- R/data_api_hmd.R
+++ R/data_api_hmd.R
@@ -14,23 +14,3 @@ gdt_hmd_mortality <- function(country) {
     password = keyring::key_get(service = "mortality.org")
   )
 }
-
-#' Get life expectancy table from Humand Mortality Database
-#'
-#' @param country A string with the country name
-#'
-#' @return A tibble
-#' @export
-gdt_hmd_lex <- function(country) {
-  gdt_hmd_e0_cached(
-    country = countrycode::countrycode(country, "country.name", "iso3c"),
-    username = keyring::key_list(service = "mortality.org")[["username"]],
-    password = keyring::key_get(service = "mortality.org")
-  ) -> data
-
-  from <- attr(data, "tsp")[[1]]
-
-  data |>
-    tibble::as_tibble(rownames = "year") |>
-    dplyr::mutate(year = as.numeric(year) + from - 1)
-}
