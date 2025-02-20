c8b656589b88af2a7d1c52b7755fc7d87bd3389c
Pascal Burkhard <pascal.burkhard@gmail.com>
2025-02-20 10:44:12 +0100

    feat: add data for OC Chine

--- R/data_doc_oc_chine.R
+++ R/data_doc_oc_chine.R
@@ -14,3 +14,17 @@
 #' }
 #' @source <http://www.stats.gov.cn/tjsj/ndsj/2019/html/E0211.jpg>
 "oc_chine_2018_sex_ratio_par_region"
+
+#' Population chinoise 2018 par sexe et régions
+#'
+#' Un tableau de la population des régions
+#' de Chine par sexe pour 2018.
+#'
+#' @format Un tibble avec 3 colonnes et 202 lignes
+#' \describe{
+#'   \item{age}{Age de la population}
+#'   \item{region}{Region `urban` ou `rural`}
+#'   \item{genderbalance}{Nombre de femme moins le nombre d'homme}
+#' }
+#' @source <http://www.stats.gov.cn/tjsj/ndsj/2019/html/E0211.jpg>
+"oc_chine_2010_gender_imbalance_by_age"
--- data-raw/oc_chine.R
+++ data-raw/oc_chine.R
@@ -1,7 +1,7 @@
 stop("This file should not be sourced!")
 
 
-# Gender balance by region ------------------------------------------------
+# Gender balance by region -----------------------------------------------
 
 readxl::read_excel(
   here::here(
@@ -38,6 +38,52 @@ readxl::read_excel(here::here("inst/extdata/oc_chine/nbs/population/2018_china_n
 
 usethis::use_data(oc_chine_2018_sex_ratio_par_region, overwrite = TRUE)
 
-# Open documentation file -------------------------------------------------
+
+# Gender imbalance by age ------------------------------------------------
+
+city <- readxl::read_excel("inst/extdata/oc_chine/nbs/gender_imbalance/A0301b.xlsx",
+  skip = 4,
+  col_types = c("text", "skip", "numeric", "numeric", "skip", "skip", "skip", "skip"),
+  col_names = c("age", "citymale", "cityfemale")
+)
+
+town <- readxl::read_excel("inst/extdata/oc_chine/nbs/gender_imbalance/A0301c.xlsx",
+  skip = 4,
+  col_types = c("text", "skip", "numeric", "numeric", "skip", "skip", "skip", "skip"),
+  col_names = c("age", "townmale", "townfemale")
+)
+
+rural <- readxl::read_excel("inst/extdata/oc_chine/nbs/gender_imbalance/A0301d.xlsx",
+  skip = 4,
+  col_types = c("text", "skip", "numeric", "numeric", "skip", "skip", "skip", "skip"),
+  col_names = c("age", "male", "female")
+)
+
+urban <- dplyr::left_join(city, town, by = "age")
+urban |>
+  dplyr::transmute(
+    age = age,
+    male = citymale + townmale,
+    female = cityfemale + townfemale
+  ) -> urban
+
+urban |>
+  dplyr::mutate(urban = male - female) |>
+  dplyr::select(age, urban) -> urban
+
+rural |>
+  dplyr::mutate(rural = male - female) |>
+  dplyr::select(age, rural) -> rural
+
+urban |>
+  dplyr::left_join(rural, by = "age") |>
+  dplyr::mutate(age = stringr::str_replace(age, "Age 100 and above", "100")) |>
+  dplyr::filter(stringr::str_detect(age, "^\\d{1,3}$")) |>
+  tidyr::pivot_longer(cols = -age, names_to = "region", values_to = "genderbalance") |>
+  dplyr::mutate(age = readr::parse_number(age)) -> oc_chine_2010_gender_imbalance_by_age
+
+usethis::use_data(oc_chine_2010_gender_imbalance_by_age, overwrite = TRUE)
+
+# Open documentation file ------------------------------------------------
 
 usethis::edit_file(here::here("R/data_doc_oc_chine.R"))
